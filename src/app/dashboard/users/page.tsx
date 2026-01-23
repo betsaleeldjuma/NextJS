0bdd9e5c153d7a6a52658c8c6af02bd13498c60a
betsaleeldjuma <djumabestaleel@gmail.com>
2026-01-23 19:24:47 +0200

    Update of user

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Users = () => {
   return (
-    <div>Users</div>
+    <div className='min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 p-8'>
+      <div className='max-w-4xl mx-auto bg-white rounded-lg shadow-xl p-8'>
+        <h1 className='text-4xl font-bold text-gray-800 mb-6'>Users</h1>
+        <p className="text-lg text-gray-600 mb-8">
+         Manage your users here. You can view detailed information or browse a list of users. 
+        </p>
+        {/* User-related section */}
+        <div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
